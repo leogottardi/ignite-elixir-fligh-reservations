f10c999ded49388fb2622f57abfa7d2966cbb9a9
Leonardo Gottardi <leomgottardi@gmail.com>
2021-10-02 18:22:28 -0300

    feat: adicionando struct de booking

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -3,7 +3,21 @@ defmodule Flightex.Bookings.Booking do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(%{
+        complete_date: complete_date,
+        local_origin: local_origin,
+        local_destination: local_destination,
+        user_id: user_id
+      }) do
+    uuid = UUID.uuid4()
+
+    {:ok,
+     %__MODULE__{
+       id: uuid,
+       complete_date: complete_date,
+       local_origin: local_origin,
+       local_destination: local_destination,
+       user_id: user_id
+     }}
   end
 end
