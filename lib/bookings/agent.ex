704e465851e7e0a1cee8bde068aeae7e0effa80e
Leonardo Gottardi <leomgottardi@gmail.com>
2021-10-04 20:46:38 -0300

    feat: adicionando funcao para recuperar reservas num periodo

--- lib/bookings/agent.ex
+++ lib/bookings/agent.ex
@@ -16,6 +16,37 @@ defmodule Flightex.Bookings.Agent do
 
   def get(uuid), do: Agent.get(__MODULE__, &get_booking(&1, uuid))
 
+  def get_between_period(from_date, to_date),
+    do:
+      Agent.get(
+        __MODULE__,
+        &Enum.map(&1, fn {_uuid, booking} ->
+          get_booking_between_period(booking, from_date, to_date)
+        end)
+      )
+
+  defp get_booking_between_period(
+         %Booking{complete_date: complete_date} = booking,
+         from_date,
+         to_date
+       ) do
+    booking_date = complete_date |> to_date()
+
+    booking_date
+    |> booking_date_in_period?(from_date, to_date, booking)
+  end
+
+  defp booking_date_in_period?(booking_date, from_date, to_date, booking) do
+    period = Date.range(from_date, to_date)
+
+    case booking_date in period do
+      true -> {:ok, booking}
+      false -> {:error, "Date not in period"}
+    end
+  end
+
+  defp to_date(naive_datetime), do: NaiveDateTime.to_date(naive_datetime)
+
   defp get_booking(state, uuid) do
     case Map.get(state, uuid) do
       nil -> {:error, "Booking not found"}
